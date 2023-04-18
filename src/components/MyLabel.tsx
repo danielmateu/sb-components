cf013876851a7f39728a0fe88020cc777ea7d563
Daniel Mateu <danielmateu86@gmail.com>
2023-04-18 13:26:24 +0200

    first commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,50 @@
+import './myLabel.css'
+
+
+export interface MyLabelProps {
+    /**
+   * Este es el mensaje a mostrar en la etiqueta
+   */
+    label: string;
+    /**
+   * Este es el tamaño de la etiqueta
+   */
+    size: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+   * Si quiere todo capitalizado
+   */
+    allCaps?: boolean;
+    /**
+   * What background color to use
+   */
+    backgroundColor?: string;
+    /**
+     * What color to use
+     * @default 'text-primary'
+     * @type 'primary' | 'secondary' | 'tertiary'
+    */
+    // color?: 'primary' | 'secondary' | 'tertiary';
+    color?: 'primary' | 'secondary' | 'tertiary';
+    /**
+       * Color personalizado de la fuente
+    */
+    fontColor?: string;
+}
+
+export const MyLabel = ({
+    allCaps = false,
+    backgroundColor = '',
+    color = 'primary',
+    label = 'No Label',
+    size = 'normal',
+    fontColor = '#1f1f1f'
+}: MyLabelProps) => {
+    return (
+        <span
+            style={{ backgroundColor, color: fontColor }}
+            className={`label ${size} text-${color}`}
+        >
+            {allCaps ? label.toUpperCase() : label}
+        </span>
+    )
+}
---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,62 @@
+// import { MyLabel } from "../../components/MyLabel";
+
+import { Meta, StoryFn } from "@storybook/react"
+import { MyLabel } from "../../components/MyLabel"
+
+export default {
+    title: 'UI/MyLabel',
+    component: MyLabel,
+    argTypes : {
+        size : {control: 'select'},
+        color : {control: 'select'},
+        fontColor : {control: 'color'},
+        backgroundColor: {control: 'color'},
+    },
+    tags: ['autodocs'], 
+} as Meta<typeof MyLabel>
+
+const Template: StoryFn<typeof MyLabel> = (args) => <MyLabel {...args}/>
+
+export const Basic = Template.bind({})
+Basic.args = {
+    label: 'Basic Label',
+    size: 'normal',
+    // allCaps: false,
+    // color: 'primary'
+}
+
+export const AllCaps = Template.bind({})
+AllCaps.args = {
+    label: 'All Caps',
+    size: 'normal',
+    allCaps: true,
+    color: 'primary'
+
+}
+
+export const Secondary = Template.bind({})
+Secondary.args = {
+    label: 'Secondary',
+    size: 'normal',
+    allCaps: false,
+    color: 'secondary'
+
+}
+
+export const Tertiary = Template.bind({})
+Tertiary.args = {
+    label: 'Tertiary',
+    size: 'normal',
+    allCaps: false,
+    color: 'tertiary'
+}
+
+// CustomFontColor
+export const CustomFontColor = Template.bind({})
+CustomFontColor.args = {
+    label: 'Custom Font Color',
+    size: 'h1',
+    fontColor: '',
+}
+
+
